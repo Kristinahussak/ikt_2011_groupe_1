6e4e7d598506c82186427488e6627219fefb2d0a
HenningFich <hefic11@student.sdu.dk>
2012-12-04 12:53:59 +0100

    IAComponent Interface

---
+++ program/acquaintance/IAComponent.java
@@ -0,0 +1,15 @@
+package acquaintance;
+
+/**
+ * IAComponent.java
+ * @author 3. Semester Projekt, Gruppe 1
+ * Anders Kold, Kristina Hussak,
+ * Henning Fich & Nicholaj Rasmussen
+ * Created on 03-12-2012
+ */
+
+public interface Component
+{    
+    
+}
+
