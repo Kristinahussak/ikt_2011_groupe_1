405da1a816479ec01219dcb2f5a5bab5686ef984
Nico P. Rasmussen <nicoprasmussen@gmail.com>
2012-12-10 16:04:55 +0100

    GUI

--- GUI/3. Semesterprojekt GUI/src/MainFrame.java
+++ GUI/3. Semesterprojekt GUI/src/MainFrame.java
@@ -1,7 +1,4 @@
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Event;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -12,9 +9,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-
 
 /**
  * MainFrame.java
@@ -26,6 +20,16 @@ import javax.swing.tree.DefaultTreeModel;
 
 public class MainFrame
 {
+    private static MainFrame instance;
+    private JPanel content;
+    GridBagConstraints c;
+    CardLayout cl;
+    
+    private String[] usernames = {"admin" , "admin","admin", "admin"};
+    private String[] passwords = {"admin" , "admin","admin", "admin"};
+    
+    private JComponent currentComponent;
+    
     private JFrame frame;
     private JMenuBar menuBar;
     private JMenu system, users, itemTypes, view, help;    
@@ -62,13 +66,38 @@ public class MainFrame
     {
         frame = new JFrame("Central Storage System - Presentation Admin");
         frame.setSize(700, 450);
+        frame.setResizable(false);
         
         this.initMenu();
         try {
-            frame.add(new SystemStart());
+            frame.setLayout(new GridBagLayout());
+            
+            content = new JPanel();
+            cl = new CardLayout();
+            content.setLayout(cl); 
+            content.add(new SystemStart(), "SystemStart");
+            content.add(new UserLogin(), "UserLogin");
+            
+            
+            c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 1;
+            c.weighty = 1;
+            c.fill = GridBagConstraints.BOTH;
+            c.anchor = GridBagConstraints.NORTHWEST;
+            frame.add(content, c);    
+            
+            
+            
+ 
         } catch (IOException ex) {
             Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
-        }                
+        }  
+        
+        this.setMenuStatus(false); //disable untill user is logged in
+        
+        
     }
     
     private void initMenu()
@@ -92,7 +121,14 @@ public class MainFrame
         system.add(login);  
         login.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,
                 Event.ALT_MASK | Event.SHIFT_MASK));
-        login.addActionListener(new NotImplementedActionListener("User login"));
+        login.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {                             
+                showCard("UserLogin");
+            }
+        });
+        
         logout = new JMenuItem("User logout",logoutImg);
         logout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
                 Event.ALT_MASK | Event.SHIFT_MASK));
@@ -203,17 +239,48 @@ public class MainFrame
         about = new JMenuItem("About", aboutImg);
         about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,
                 Event.ALT_MASK | Event.CTRL_MASK));
-        about.addActionListener(new NotImplementedActionListener("About"));
-        help.add(about);        
+        about.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                About temp = new About();
+            }
+        });
+        help.add(about);  
         
         //Set menu-bar on frame
         frame.setJMenuBar(menuBar);
         
     }
     
+    public void showCard(String cardName)
+    {
+        cl.show(content,cardName);
+    }
+    
+    public static MainFrame getInstance()
+    {
+        if(instance == null){instance = new MainFrame();}
+        return instance;
+    }
+    
+    
     public JFrame getFrame()
     {
         return frame;
+    }
+    
+    private void setMenuStatus(boolean state)
+    {
+        help.setEnabled(state);         
+        users.setEnabled(state); 
+        itemTypes.setEnabled(state); 
+        view.setEnabled(state); 
+        help.setEnabled(state);        
+        systemInfo.setEnabled(state);
+        logout.setEnabled(state);
+        startSystem.setEnabled(state); 
+        stopSystem.setEnabled(state);
     }
     
     public class NotImplementedActionListener implements ActionListener
@@ -230,17 +297,18 @@ public class MainFrame
             JOptionPane.showMessageDialog(frame,
                     "This function has not yet been implemented",
                     this.title,
-                    JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.ERROR_MESSAGE);            
         }
         
     }
+    
     
     public class SystemStart extends JPanel
     {
         private BufferedImage image;
 
         public SystemStart() throws IOException {
-           image = ImageIO.read(new File("images/logo.png"));
+           image = ImageIO.read(new File("images/logo.png"));           
         }
         
         @Override
@@ -249,5 +317,186 @@ public class MainFrame
         g.drawImage(image, 0, 0,this.getWidth(),this.getHeight(), null);
         }
         
+    }
+    
+    public class About extends JFrame 
+    {
+        private BufferedImage image;
+        boolean initiated = false;
+        public About()
+        {
+            JFrame frame = new JFrame();
+            try {
+                image = ImageIO.read(new File("images/helpMenu/aboutBackground.png"));
+            } catch (IOException ex) {
+                Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            this.setSize(350, 180);
+            this.setLocationRelativeTo(frame);
+            this.setVisible(true);
+            this.setResizable(false);            
+            this.setTitle("Central Storage System v. 1.0");
+            
+            DrawablePanel panel = new DrawablePanel(); 
+         
+            this.add(panel);
+            this.setAlwaysOnTop(true);
+            
+            
+            JLabel textLabel = new JLabel("",JLabel.CENTER);
+            textLabel.setVerticalAlignment(SwingConstants.TOP);
+            textLabel.setFont(new Font("Garamond", Font.BOLD, 25));
+            textLabel.setText("Central Storage System v. 1.0");
+            textLabel.setForeground(new Color(81,63,46));
+            panel.add(textLabel);
+            
+            JLabel nameLabel = new JLabel("",SwingConstants.CENTER);
+            
+            nameLabel.setFont(new Font("Garamond", Font.BOLD, 15));
+            nameLabel.setText("<html><center><br>Nicholaj P. Rasmussen <br>"
+                    + "Kristina Hussak <br>"
+                    + "Henning Fich <br>"
+                    + "Anders Kold</center></html>");
+            nameLabel.setForeground(new Color(81,63,46));
+            nameLabel.setHorizontalTextPosition(JLabel.CENTER);
+            
+            panel.add(nameLabel);
+            
+                    
+        }
+        
+        class DrawablePanel extends JPanel {
+            
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+            }
+        }
+        
+        
+        
+    }
+    
+    public class UserLogin extends JPanel
+    {
+        private BufferedImage image;
+        private ImageIcon loginButtonImg = new ImageIcon("images/systemMenu/loginButton.png");
+        private ImageIcon loginButtonDownImg = new ImageIcon("images/systemMenu/loginButtonDown.png");
+        private ImageIcon clearButtonImg = new ImageIcon("images/systemMenu/clearButton.png");
+        private ImageIcon clearButtonDownImg = new ImageIcon("images/systemMenu/clearButtonDown.png");
+        public UserLogin()
+        {   
+            
+            //300,225
+            try {
+                image = ImageIO.read(new File("images/systemMenu/loginBackground.png"));
+            } catch (IOException ex) {
+                Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            final JTextField usernameField;    
+            final JPasswordField passwordField;
+            JButton loginButton, clearButton;
+            this.setLayout(null);
+            
+            
+            usernameField = new JTextField(); 
+            usernameField.setBounds(260,124,215,30);
+            usernameField.setBorder(null);
+            usernameField.setOpaque(false);
+            usernameField.setFont(new Font("SansSerif", Font.BOLD, 14));
+            this.add(usernameField);
+            
+            passwordField = new JPasswordField();
+            passwordField.setBounds(260,175,215,30);
+            passwordField.setBorder(null);
+            passwordField.setOpaque(false);
+            passwordField.setFont(new Font("SansSerif", Font.BOLD, 14));            
+            this.add(passwordField);
+            
+            loginButton = new JButton();
+            loginButton.setBounds(203,254,121,42);
+            loginButton.setIcon(loginButtonImg);
+            loginButton.setPressedIcon(loginButtonDownImg);
+            loginButton.setBorder(null);
+            loginButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) 
+                {
+                    String inputUsername = usernameField.getText();
+                    String inputPassword = passwordField.getText();
+                            
+                    
+                    boolean usernameCorrect = false;
+                    boolean passwordCorrect = false;
+                    for (int i = 0; i < usernames.length && !usernameCorrect; i++)
+                    {
+                        if(usernames[i].equals(inputUsername))
+                        {
+                            usernameCorrect = true;
+                        }                        
+                    }
+                    
+                    for (int j = 0; j < passwords.length && !passwordCorrect; j++)
+                    {
+                        if(passwords[j].equals(inputPassword))
+                        {
+                            passwordCorrect = true;
+                        }                        
+                    }
+                    
+                    if(usernameCorrect && passwordCorrect)
+                    {
+                        setMenuStatus(true);
+                        
+                        JOptionPane.showMessageDialog(frame,
+                                "You know have access to the system",
+                                "Login successful",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        showCard("SystemStart");
+                    }     
+                    
+                    else{
+                        JOptionPane.showMessageDialog(frame,
+                                "Incorrect login information",
+                                "Error in login",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                    
+                }
+            });
+            this.add(loginButton);
+            
+            clearButton = new JButton();
+            clearButton.setBounds(370,254,121,42);
+            clearButton.setIcon(clearButtonImg);
+            clearButton.setPressedIcon(clearButtonDownImg);
+            clearButton.setBorder(null);
+            clearButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    usernameField.setText("");
+                    passwordField.setText("");
+                }
+            });
+            this.add(clearButton);
+        }
+        
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+        }
+    }
+    
+    public class createUser extends JPanel
+    {
+        public createUser()
+        {
+            
+            
+        }
     }
 }
