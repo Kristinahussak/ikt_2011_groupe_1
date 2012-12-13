74f764cab2e84b11e8ab4ffe61cafe68577de076
Nico P. Rasmussen <nicoprasmussen@gmail.com>
2012-12-13 16:21:00 +0100

    Mere Rettelse

--- program/entity/EFacade.java
+++ program/entity/EFacade.java
@@ -68,25 +68,9 @@ public class EFacade
         
         order3.add(item6);
         
-        order1.setState(IAComponent.ORDER_CLOSED);
-        
         orders.add(order1);
         orders.add(order2);
         orders.add(order3);
-        orders.add(order3);
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order3);
-        orders.add(order3);
-        orders.add(order3);
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order2);
-        orders.add(order2);
-        orders.add(order2);
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order2);
         
     }
     
